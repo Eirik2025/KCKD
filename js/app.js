1dd79edc71ec044fa71c5c1153a2586c7a165654
MillerC97 <millercody1997@hotmail.com>
2026-04-16 22:26:00 -0500

    initial home page setup

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Placeholder for now
+console.log('Klandestine initialized');
+
+// Countdown placeholder
+const countdownEl = document.getElementById('countdown');
+countdownEl.innerHTML = '<p class="countdown">Something stirs...</p>';
